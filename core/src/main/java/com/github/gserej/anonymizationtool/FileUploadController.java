eab322d9099509920d9d4d4351786cf930bfcd04
gserej <gserej@gmail.com>
2019-08-17 16:36:49 +0200

    backup 17-08-2019

---
+++ core/src/main/java/com/github/gserej/anonymizationtool/FileUploadController.java
@@ -0,0 +1,85 @@
+package com.github.gserej.anonymizationtool;
+
+import com.github.gserej.anonymizationtool.storage.StorageFileNotFoundException;
+import com.github.gserej.anonymizationtool.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/")
+    public String listUploadedFiles(Model model) throws IOException {
+
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+
+        return "pageviewer";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        File file2 = storageService.loadAsFile(file.getOriginalFilename());
+
+        try {
+            PrintDrawLocations.PrintDrawLocation(file2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping(value = "/api")
+    @ResponseBody
+    public String postJson(@RequestBody List<Rectangle> rectangles, HttpServletRequest request) {
+        System.out.println(rectangles.toString());
+        return null;
+    }
+
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+}
---
+++ core/src/main/java/com/github/gserej/anonymizationtool/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.github.gserej.anonymizationtool.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
