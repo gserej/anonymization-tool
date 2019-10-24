0cc6f92b802021bede7146295bb5dd84b4172c37
gserej <gserej@gmail.com>
2019-10-24 12:54:21 +0200

    removed core module to temporally fix the problem with running the  spring-boot:run command, changed java version to 11

--- src/main/java/com/github/gserej/anonymizationtool/controllers/RectanglesHandlingController.java
+++ src/main/java/com/github/gserej/anonymizationtool/controllers/RectanglesHandlingController.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -25,6 +28,7 @@ public class RectanglesHandlingController {
     public RectanglesHandlingController(MarkedRectanglesProcessingService markedRectanglesProcessingService) {
         this.markedRectanglesProcessingService = markedRectanglesProcessingService;
     }
+
 
     @PostMapping(value = "/api")
     public String postJson(@RequestBody List<RectangleBox> rectangleBoxesMarked, RedirectAttributes redirectAttributes) {
