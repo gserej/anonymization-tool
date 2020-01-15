b18bfa04052970425d2f039bc0f40ca7bef92281
gserej <gserej@gmail.com>
2020-01-15 11:50:07 +0100

    extract method to eliminate code duplication

--- src/main/java/com/github/gserej/anonymizationtool/datacategory/impl/NumberTypeValidationServiceImpl.java
+++ src/main/java/com/github/gserej/anonymizationtool/datacategory/impl/NumberTypeValidationServiceImpl.java
@@ -17,73 +17,60 @@ public class NumberTypeValidationServiceImpl implements NumberTypeValidationServ
         }
 
         int[] weights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+        int checksumNumber = Integer.parseInt(pesel.substring(10, 11));
 
         int sum = 0;
-
         for (int i = 0; i < 10; i++) {
             sum += Integer.parseInt(pesel.substring(i, i + 1)) * weights[i];
         }
-        int checksum = Integer.parseInt(pesel.substring(10, 11));
-
         sum %= 10;
         sum = 10 - sum;
         sum %= 10;
 
-        return (sum == checksum);
+        return (sum == checksumNumber);
     }
 
     @Override
     public boolean isValidNIP(String nip) {
-        int nsize = nip.length();
-        if (nsize != 10) {
+        int nipLength = nip.length();
+        if (nipLength != 10) {
             return false;
         }
         if (!StringUtils.isNumeric(nip)) {
             return false;
         }
         int[] weights = {6, 5, 7, 2, 3, 4, 5, 6, 7};
-        int j, sum = 0, control;
-        int csum = Integer.parseInt(nip.substring(nsize - 1));
-        for (int i = 0; i < nsize - 1; i++) {
-            char c = nip.charAt(i);
-            j = Integer.parseInt(String.valueOf(c));
-            sum += j * weights[i];
-        }
-        control = sum % 11;
-        return (control == csum);
-
+        int checksumNumber = Integer.parseInt(nip.substring(nipLength - 1));
+        return (calculateChecksum(nip, nipLength, weights) == checksumNumber);
     }
 
     @Override
     public boolean isValidREGON(String regon) {
-
-        int rsize = regon.length();
-        if (!((rsize == 9) || (rsize == 14))) {
+        int regonLength = regon.length();
+        if (!((regonLength == 9) || (regonLength == 14))) {
             return false;
         }
         if (!StringUtils.isNumeric(regon)) {
             return false;
         }
-
         int[] weights = {8, 9, 2, 3, 4, 5, 6, 7};
         int[] weights14 = {2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8};
-
-        if (rsize == 14) {
+        if (regonLength == 14) {
             weights = weights14;
         }
-
-        int j, sum = 0, control;
-        int csum = Integer.parseInt(regon.substring(rsize - 1));
-        for (int i = 0; i < rsize - 1; i++) {
-            char c = regon.charAt(i);
-            j = Integer.parseInt(String.valueOf(c));
-            sum += j * weights[i];
+        int checksumNumber = Integer.parseInt(regon.substring(regonLength - 1));
+        int calculatedChecksum = calculateChecksum(regon, regonLength, weights);
+        if (calculatedChecksum == 10) {
+            calculatedChecksum = 0;
         }
+        return (calculatedChecksum == checksumNumber);
+    }
 
-        control = sum % 11;
-        if (control == 10) {
-            control = 0;
+    private int calculateChecksum(String stringNumber, int length, int[] weights) {
+        int sum = 0;
+        for (int i = 0; i < length - 1; i++) {
+            sum += Integer.parseInt(String.valueOf(stringNumber.charAt(i))) * weights[i];
         }
-        return (control == csum);
+        return sum % 11;
     }
 }
