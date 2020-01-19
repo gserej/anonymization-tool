38ae36482cbaee96053df53c704b79c03ad65cd8
gserej <gserej@gmail.com>
2020-01-19 13:14:07 +0100

    add custom exception to controller methods

--- src/main/java/com/github/gserej/anonymizationtool/rectangles/RectangleController.java
+++ src/main/java/com/github/gserej/anonymizationtool/rectangles/RectangleController.java
@@ -1,6 +1,7 @@
 package com.github.gserej.anonymizationtool.rectangles;
 
 import com.github.gserej.anonymizationtool.document.Document;
+import com.github.gserej.anonymizationtool.document.DocumentNotFoundException;
 import com.github.gserej.anonymizationtool.document.DocumentRepository;
 import com.github.gserej.anonymizationtool.messages.CurrentMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +15,6 @@ import java.util.UUID;
 public class RectangleController {
 
     private final MarkedRectanglesProcessingService markedRectanglesProcessingService;
-
     private final DocumentRepository documentRepository;
 
     public RectangleController(MarkedRectanglesProcessingService markedRectanglesProcessingService,
@@ -23,19 +23,19 @@ public class RectangleController {
         this.documentRepository = documentRepository;
     }
 
-
     @PostMapping(value = "/api/rectangles/{uuid}")
-    public String postRectangles(@RequestBody Set<RectangleBox> rectangleBoxesMarked, @PathVariable("uuid") UUID uuid) {
+    public String postRectangles(@RequestBody Set<RectangleBox> rectangleBoxesMarked, @PathVariable("uuid") UUID uuid)
+            throws DocumentNotFoundException {
         markedRectanglesProcessingService.processReceivedRectangleSet(rectangleBoxesMarked, uuid);
-        Document document = documentRepository.findById(uuid).orElseThrow();
+        Document document = documentRepository.findById(uuid).orElseThrow(DocumentNotFoundException::new);
         document.setCurrentMessage(CurrentMessage.DOWNLOAD_LINK_READY.getValue());
         documentRepository.save(document);
         return "link created";
     }
 
     @GetMapping("/api/rectangles/{uuid}")
-    public Set<RectangleBox> getRectangles(@PathVariable("uuid") UUID uuid) {
-        Document document = documentRepository.findById(uuid).orElseThrow();
+    public Set<RectangleBox> getRectangles(@PathVariable("uuid") UUID uuid) throws DocumentNotFoundException {
+        Document document = documentRepository.findById(uuid).orElseThrow(DocumentNotFoundException::new);
         return document.getParsedRectangles();
     }
 }
